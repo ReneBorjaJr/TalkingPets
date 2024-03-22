29444333005d81311a0569b11f9d46dabf681cfc
ReneBorjaJr <rene.borja99@gmail.com>
2024-03-22 00:46:09 -0400

    Talking pets

---
+++ src/main/java/io/zipcoder/polymorphism/Bird.java
@@ -0,0 +1,11 @@
+package io.zipcoder.polymorphism;
+
+public class Bird extends Pet{
+    public Bird(String name){
+        super(name);
+    }
+    @Override
+    public String speak() {
+        return "Tweet! I'm a bird!";
+    }
+}
--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,59 @@
 package io.zipcoder.polymorphism;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MainApplication {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Pet> pets = new ArrayList<>();
+
+        System.out.println("How many pets do you have?");
+        int numberOfPets = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline left-over
+
+//        String[] petTypes = new String[numberOfPets];
+//        String[] petNames = new String[numberOfPets];
+
+        for (int i = 0; i < numberOfPets; i++) {
+            System.out.println("What kind of pet is pet number " + (i + 1) + "?");
+            String petType = scanner.nextLine();
+
+            System.out.println("What is the name of this pet?");
+            String petName = scanner.nextLine();
+
+            switch (petType.toLowerCase()) {
+                case "dog":
+                    pets.add(new Dog(petName));
+                    break;
+                case "cat":
+                    pets.add(new Cat(petName));
+                    break;
+                case "bird":
+                    pets.add(new Bird(petName));
+                    break;
+                default:
+                    System.out.println("Unsupported pet type.");
+                    break;
+            }
+        }
+        for (Pet pet : pets) {
+            System.out.println(pet.getName() + " says " + pet.speak());
+        }
+
+        // Print out the list of pets
+//        for (int i = 0; i < numberOfPets; i++) {
+//            System.out.println("Pet #" + (i + 1) + ": " + petTypes[i] + " named " + petNames[i]);
+//        }
+
+
+    }
+
+    }
+
+
+
+
+
+
